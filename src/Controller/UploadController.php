ad929d60df5f48cf55bc2aa41288728b44d888c1
Thibault Marquand - Mediashare <mediashare.supp@gmail.com>
2020-01-28 19:32:52 +0100

    Upload files update

--- src/Controller/UploadController.php
+++ src/Controller/UploadController.php
@@ -17,15 +17,31 @@ class UploadController extends AbstractController
         if (count($files) > 0):
             $em = $this->getDoctrine()->getManager();
             $stockage = $this->getParameter('kernel.project_dir').$this->getParameter('stockage');
-            foreach ($files as $file) {
-                $fileSystem = new FileSystemApi();
+            $fileSystem = new FileSystemApi();
+            $size = 0;
+            foreach ($files as $index => $file) {
                 $file = $fileSystem->upload($file, $stockage);
                 $em->persist($file);
                 $em->flush();
+                
+                $results[] = [
+                    'id' => $file->getId(),
+                    'name' => $file->getName(),
+                    'path' => $file->getPath(),
+                    'mimeType' => $file->getMimeType(),
+                    'size' => $fileSystem->getSizeReadable($file->getSize())
+                ];
+                $size += $file->getSize();
             }
+            // Response
             return $this->json([
                 'status' => 'success',
                 'message' => 'Your file(s) was uploaded.',
+                'files' => [
+                    'counter' => count($results),
+                    'size' => $fileSystem->getSizeReadable($size),
+                    'results' => $results
+                ]
             ]);
         endif;
         return $this->json([
