738da1e55bdb6220a36c7763a5767b1ed8e22604
Thibault Marquand - Mediashare <mediashare.supp@gmail.com>
2020-01-28 19:43:39 +0100

    Cleaner

---
+++ clean.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Clear file database & remove this from the stockage.
+ */
+
+$files = getFiles();
+foreach ($files as $file) {
+    $id = $file->id;
+    remove_file($id);
+}
+function getFiles(): array {
+    $request = curl_init();
+    curl_setopt($request, CURLOPT_URL,"http://localhost:8001/");
+    // output the response
+    curl_setopt($request, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($request);
+    $response = json_decode($response);
+    // close the session
+    curl_close($request);
+    
+    return $response->files->results;
+}
+function remove_file(string $id) {
+    $request = curl_init();
+    curl_setopt($request, CURLOPT_URL,"http://localhost:8001/remove/".$id);
+    // output the response
+    curl_setopt($request, CURLOPT_RETURNTRANSFER, false);
+    $response = curl_exec($request);
+    // close the session
+    curl_close($request);
+}
--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
 // send a file
 $request = curl_init();
-curl_setopt($request, CURLOPT_URL,"http://localhost:8000/upload");
+curl_setopt($request, CURLOPT_URL,"http://localhost:8001/upload");
 curl_setopt($request, CURLOPT_POST, true);
 curl_setopt(
     $request,
